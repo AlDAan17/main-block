47b88d3dc45deba44f20b2849922d10a8db492b4
Aldani <kototi17@gmail.com>
2020-04-04 13:37:45 +0300

    added all blocks

--- js/app.js
+++ js/app.js
@@ -1,21 +1,34 @@
+// slide logo
 const btnText = document.getElementById("swiper-showmore");
 const btnImg = document.querySelector(".swiper-more-button");
 const btnMoreLogos = document.querySelector("#btn-more-logos");
 const slide = document.getElementsByClassName("logos");
-
-const slideTech = document.getElementsByClassName("slide-technics");
+// slide tech
+const slideTech = document.getElementsByClassName("technics");
 const btnMoreTech = document.querySelector("#btn-more-technics");
 const btnTextTech = document.getElementById("swiper-showmore-tech");
 const btnImgTech = document.getElementById("btn-img-tech");
+// burger
+const burger = document.querySelector('.burger');
+const navbar = document.querySelector('.navbar');
+const content = document.querySelector('.content');
+const header = document.querySelector('header');
+const closeIcon = document.querySelector('.navbar__content-header-close');
 
 if (document.body.clientWidth < 768) {
-    let mySwiper_test = new Swiper('.swiper-container', {
+    let mySwiper_test = new Swiper('.swiper-container-logo', {
         slidesPerView: 'auto',
         pagination: {
             el: '.swiper-pagination'
         }
     });
     let test = new Swiper('.swiper-container-technics',{
+        slidesPerView: 'auto',
+        pagination: {
+            el: '.swiper-pagination'
+        }
+    });
+    let prices = new Swiper('.swiper-container-prices',{
         slidesPerView: 'auto',
         pagination: {
             el: '.swiper-pagination'
@@ -48,4 +61,16 @@ btnMoreTech.addEventListener('click', function () {
             btnImgTech.style.transform = "rotate(180deg)";
         }
     }
+});
+
+burger.addEventListener('click', () => {
+    navbar.classList.add('navbar--active');
+    content.classList.add('content--active');
+    header.classList.add('header--active');
+});
+
+closeIcon.addEventListener('click', () => {
+    navbar.classList.remove('navbar--active');
+    content.classList.remove('content--active');
+    header.classList.remove('header--active');
 });
